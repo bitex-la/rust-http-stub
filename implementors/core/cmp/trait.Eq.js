9be94cd22bd6a128b7904abac99b851a5073b8d0
Nubis <yo@nubis.im>
2016-04-22 13:14:19 -0300

    first docs commit

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['http_stub'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='http_stub/enum.Method.html' title='http_stub::Method'>Method</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='http_stub/enum.TopLevel.html' title='http_stub::TopLevel'>TopLevel</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='http_stub/enum.SubLevel.html' title='http_stub::SubLevel'>SubLevel</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/convert/trait.AsRef.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['http_stub'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html' title='core::convert::AsRef'>AsRef</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>&gt; for <a class='enum' href='http_stub/enum.Method.html' title='http_stub::Method'>Method</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
